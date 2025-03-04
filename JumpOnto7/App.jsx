80c22898bd156dfce2989ea4af17da70fc47e6da
ranjeet yadav <147843523+ranjeetonGitthub@users.noreply.github.com>
2025-03-04 23:07:03 +0530

    Add files via upload

--- JumpOnto7/App.jsx
+++ JumpOnto7/App.jsx
@@ -1,30 +1,58 @@
-import { useState } from 'react'
-
-import './App.css'
-import { BrowserRouter,Routes, Route } from 'react-router-dom'
-import { dashboard } from './Components/dashboard'
-import { landing } from './Components/landing'
+import { useContext, useMemo, useState } from "react"
+import { CountContext } from "./context";
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { countAtom, evenSelector } from "./store/atoms/count";
 
 function App() {
-
-
   return (
     <div>
-
-       <div style={{background: "black", color: "white"}}>
-        Hi this is the topbar
-
-       </div>
-    
-    <BrowserRouter>
-    <Routes>
-      <Route  path="/dashboard" element={<dashboard/> }/>
-      <Route  path="/" element={<landing/> }/>
-    </Routes>
-     
-    </BrowserRouter>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
     </div>
   )
+}
+
+function Count() {
+  console.log("re-render");
+  return <div>
+    <CountRenderer />
+    <Buttons />
+  </div>
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  
+  return <div>
+    <b>
+      {count}
+    </b>
+    <EvenCountRenderer />
+  </div>
+}
+
+function EvenCountRenderer() {
+  const isEven = useRecoilValue(evenSelector);
+
+  return <div>
+    {isEven ? "It is even" : null}
+  </div>
+}
+
+function Buttons() {
+  const setCount = useSetRecoilState(countAtom);
+  console.log("buttons re-rendererd");
+
+  return <div>
+    <button onClick={() => {
+      setCount(count => count + 1)
+    }}>Increase</button>
+
+    <button onClick={() => {
+      setCount(count => count - 1)
+    }}>Decrease</button>
+  </div>
 }
 
 export default App
