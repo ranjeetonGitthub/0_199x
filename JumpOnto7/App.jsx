d3e0f30664fbcf0268bdcf265d0738ceae86695b
ranjeet yadav <147843523+ranjeetonGitthub@users.noreply.github.com>
2025-03-07 23:04:47 +0530

    Add files via upload

--- JumpOnto7/App.jsx
+++ JumpOnto7/App.jsx
@@ -1,58 +1,32 @@
-import { useContext, useMemo, useState } from "react"
-import { CountContext } from "./context";
-import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import { countAtom, evenSelector } from "./store/atoms/count";
+import { useState } from 'react'
+import { RecoilRoot, useRecoilValue } from 'recoil'
+import { networkAtom } from './atom'
+// RecoilRoot
+
+
 
 function App() {
+ 
+  return  <RecoilRoot>
+    <mainApp />
+    </RecoilRoot>
+    
+}
+ function mainApp() { 
+  const networkNotificationCount = useRecoilValue(networkAtom)
+
   return (
-    <div>
-      <RecoilRoot>
-        <Count />
-      </RecoilRoot>
-    </div>
+    <>
+     <button>Home</button>
+
+     <button>My Network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+     <button>Jobs</button>
+     <button>Messaging</button>
+     <button>Notification</button>
+     <button>ME</button>
+    </>
   )
 }
 
-function Count() {
-  console.log("re-render");
-  return <div>
-    <CountRenderer />
-    <Buttons />
-  </div>
-}
-
-function CountRenderer() {
-  const count = useRecoilValue(countAtom);
-  
-  return <div>
-    <b>
-      {count}
-    </b>
-    <EvenCountRenderer />
-  </div>
-}
-
-function EvenCountRenderer() {
-  const isEven = useRecoilValue(evenSelector);
-
-  return <div>
-    {isEven ? "It is even" : null}
-  </div>
-}
-
-function Buttons() {
-  const setCount = useSetRecoilState(countAtom);
-  console.log("buttons re-rendererd");
-
-  return <div>
-    <button onClick={() => {
-      setCount(count => count + 1)
-    }}>Increase</button>
-
-    <button onClick={() => {
-      setCount(count => count - 1)
-    }}>Decrease</button>
-  </div>
-}
 
 export default App
